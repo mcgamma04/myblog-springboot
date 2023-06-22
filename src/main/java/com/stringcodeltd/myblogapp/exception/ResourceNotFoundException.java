93e335cc019543662dde92e83b45fa0cbb5dab64
Adebayo Michael <mcgamma04@yahoo.com>
2023-06-22 05:57:21 +0100

    created not found exception class

--- src/main/java/com/stringcodeltd/myblogapp/exception/ResourceNotFoundException.java
+++ src/main/java/com/stringcodeltd/myblogapp/exception/ResourceNotFoundException.java
@@ -1,4 +1,30 @@
 package com.stringcodeltd.myblogapp.exception;
 
-public class ResourceNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+    private String resourceName;
+    private String fleldName;
+    private String fieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fleldName, String fieldValue) {
+        super(String.format("%s not found with %s: %s",resourceName,fleldName,fieldValue));//post not found with id:1
+        this.resourceName = resourceName;
+        this.fleldName = fleldName;
+        this.fieldValue = fieldValue;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public String getFleldName() {
+        return fleldName;
+    }
+
+    public String getFieldValue() {
+        return fieldValue;
+    }
 }
