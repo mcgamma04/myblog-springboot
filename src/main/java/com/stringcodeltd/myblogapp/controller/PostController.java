a657436a2bf6a3b9043276031366bde81c8a236a
Adebayo Michael <mcgamma04@yahoo.com>
2023-06-22 20:04:56 +0100

    get all record

--- src/main/java/com/stringcodeltd/myblogapp/controller/PostController.java
+++ src/main/java/com/stringcodeltd/myblogapp/controller/PostController.java
@@ -4,10 +4,9 @@ import com.stringcodeltd.myblogapp.dto.PostDTO;
 import com.stringcodeltd.myblogapp.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -20,6 +19,11 @@ public class PostController {
     @PostMapping
     public ResponseEntity<PostDTO> createPost(@RequestBody PostDTO postDTO) {
      return  new ResponseEntity<>(postService.createPost(postDTO), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<PostDTO> getAll(){
+         return postService.getAllPost();
     }
 
 }
