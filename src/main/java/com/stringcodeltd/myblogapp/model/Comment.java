844e64a6f6e4f3cd31d52ddc28b3fb4c62e14b91
Adebayo Michael <mcgamma04@yahoo.com>
2023-06-25 18:14:32 +0100

    added many to one to comment

--- src/main/java/com/stringcodeltd/myblogapp/model/Comment.java
+++ src/main/java/com/stringcodeltd/myblogapp/model/Comment.java
@@ -11,12 +11,16 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "comments")
-public class Comments {
+public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private long id;
     private String name;
     private String email;
     private String body;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
 
 }
