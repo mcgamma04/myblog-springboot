cee94e18966840baa762aaf4654e3122e74a9e0b
Adebayo Michael <mcgamma04@yahoo.com>
2023-06-26 06:04:23 +0100

    comment service

---
+++ src/main/java/com/stringcodeltd/myblogapp/service/CommentService.java
@@ -0,0 +1,8 @@
+package com.stringcodeltd.myblogapp.service;
+
+import com.stringcodeltd.myblogapp.dto.CommentDTO;
+
+public interface CommentService {
+    CommentDTO createComment(long postId, CommentDTO commentDTO);
+
+}
