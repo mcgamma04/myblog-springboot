4268de027ed926ef87e98a9e637c930223b9d098
Adebayo Michael <mcgamma04@yahoo.com>
2023-06-26 07:03:47 +0100

    done with create comment logic

--- src/main/java/com/stringcodeltd/myblogapp/controller/CommentController.java
+++ src/main/java/com/stringcodeltd/myblogapp/controller/CommentController.java
@@ -1,9 +1,25 @@
 package com.stringcodeltd.myblogapp.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.stringcodeltd.myblogapp.dto.CommentDTO;
+import com.stringcodeltd.myblogapp.service.CommentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/v1")
 public class CommentController {
+
+    private CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @GetMapping("/posts/{postid}/comments")
+    public ResponseEntity<CommentDTO> createComment(@PathVariable(value = "postid") long postId, @RequestBody CommentDTO commentDTO){
+        CommentDTO commentnew = commentService.createComment(postId, commentDTO);
+        return new ResponseEntity<>(commentnew, HttpStatus.CREATED);
+
+    }
 }
