9b10c1b156bc55363b579773ab25231a177d7e3f
Adebayo Michael <mcgamma04@yahoo.com>
2023-06-26 06:46:04 +0100

    create comment service implementation

---
+++ src/main/java/com/stringcodeltd/myblogapp/controller/CommentController.java
@@ -0,0 +1,9 @@
+package com.stringcodeltd.myblogapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class CommentController {
+}
