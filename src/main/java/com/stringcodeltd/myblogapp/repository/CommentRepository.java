0655749a18dace35b6cdb69dd3eac73435963677
Adebayo Michael <mcgamma04@yahoo.com>
2023-06-26 15:59:56 +0100

    find all posts

--- src/main/java/com/stringcodeltd/myblogapp/repository/CommentRepository.java
+++ src/main/java/com/stringcodeltd/myblogapp/repository/CommentRepository.java
@@ -3,5 +3,10 @@ package com.stringcodeltd.myblogapp.repository;
 import com.stringcodeltd.myblogapp.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment,Long> {
+
+    List<Comment> findByPostId(long postid);
+
 }
