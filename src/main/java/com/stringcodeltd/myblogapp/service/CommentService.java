1dae03e7d4ee30f02dd8e4061adc75614f8d6d66
Adebayo Michael <mcgamma04@yahoo.com>
2023-06-27 15:12:09 +0100

    added get comment by postid to service layer

--- src/main/java/com/stringcodeltd/myblogapp/service/CommentService.java
+++ src/main/java/com/stringcodeltd/myblogapp/service/CommentService.java
@@ -2,8 +2,11 @@ package com.stringcodeltd.myblogapp.service;
 
 import com.stringcodeltd.myblogapp.dto.CommentDTO;
 
+import java.util.List;
+
 public interface CommentService {
     CommentDTO createComment(long postId, CommentDTO commentDTO);
+    List<CommentDTO> getCommentByPostId(long postId);
 
 
 
