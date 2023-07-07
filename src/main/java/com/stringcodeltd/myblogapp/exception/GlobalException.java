27bc453e87b4e1a5b2b0d1246514edda7ca4f3ac
Adebayo Michael <mcgamma04@yahoo.com>
2023-07-07 05:31:53 +0100

    handle blogapi exception globally

--- src/main/java/com/stringcodeltd/myblogapp/exception/GlobalException.java
+++ src/main/java/com/stringcodeltd/myblogapp/exception/GlobalException.java
@@ -19,4 +19,10 @@ public class GlobalException {
         ErrorDetails errorDetails = new ErrorDetails(new Date(),exception.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(BlogApiException.class)
+    public ResponseEntity<ErrorDetails> handleBlogApiException(BlogApiException blogApiException, WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(),blogApiException.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
 }
