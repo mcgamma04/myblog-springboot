317691057a1400b868b9f8b10ceac56532b55626
Adebayo Michael <mcgamma04@yahoo.com>
2023-07-07 06:08:26 +0100

    handled global exception hanler

--- src/main/java/com/stringcodeltd/myblogapp/exception/GlobalException.java
+++ src/main/java/com/stringcodeltd/myblogapp/exception/GlobalException.java
@@ -25,4 +25,9 @@ public class GlobalException {
         ErrorDetails errorDetails = new ErrorDetails(new Date(),blogApiException.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception,WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(),exception.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
