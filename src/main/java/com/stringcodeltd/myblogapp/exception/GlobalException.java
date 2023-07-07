b19278da806f62d0398dd8eb01d130351f743e20
Adebayo Michael <mcgamma04@yahoo.com>
2023-07-07 05:28:02 +0100

    handle it not found

---
+++ src/main/java/com/stringcodeltd/myblogapp/exception/GlobalException.java
@@ -0,0 +1,22 @@
+package com.stringcodeltd.myblogapp.exception;
+
+import com.stringcodeltd.myblogapp.dto.ErrorDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalException {
+
+    //handle specific exception
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(),exception.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+}
