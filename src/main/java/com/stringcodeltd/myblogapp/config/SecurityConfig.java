fc8b8d4b9672e8dc9b09763cb0c148db41b20544
Adebayo Michael <mcgamma04@yahoo.com>
2023-07-09 04:44:42 +0100

    set up user details in application

--- src/main/java/com/stringcodeltd/myblogapp/config/SecurityConfig.java
+++ src/main/java/com/stringcodeltd/myblogapp/config/SecurityConfig.java
@@ -3,6 +3,13 @@ package com.stringcodeltd.myblogapp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 public class SecurityConfig {
@@ -15,5 +22,25 @@ public class SecurityConfig {
                             .authenticated()
                     ).httpBasic(Customizer.withDefaults());
     return http.build();
+    }
+
+//    @Bean
+//    public UserDetailsService userDetails(){
+//        UserDetails bloguser     = User.builder()
+//                .username("mcgamma04")
+//                .password(passwordEncoder().encode("mike"))
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("admin"))
+//                .roles("ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(bloguser,admin);
+//    }
+
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
