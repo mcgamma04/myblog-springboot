87ef778958c4ff2194d5173a3ffda569afb1d2ef
Adebayo Michael <mcgamma04@yahoo.com>
2023-07-11 13:08:58 +0100

    added category controller

---
+++ src/main/java/com/stringcodeltd/myblogapp/controller/CategoryController.java
@@ -0,0 +1,21 @@
+package com.stringcodeltd.myblogapp.controller;
+
+import com.stringcodeltd.myblogapp.dto.CategoryDTO;
+import com.stringcodeltd.myblogapp.service.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/category")
+public class CategoryController {
+private CategoryService categoryService;
+    @PostMapping
+    public ResponseEntity<CategoryDTO> createCategory(@RequestBody CategoryDTO categoryDTO){
+        CategoryDTO comment = categoryService.createComment(categoryDTO);
+        return new ResponseEntity<>(comment, HttpStatus.CREATED);
+    }
+}
