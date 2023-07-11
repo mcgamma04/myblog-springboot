c9ec19abdd7f9603144771f4e758e60a8f327fba
Adebayo Michael <mcgamma04@yahoo.com>
2023-07-11 06:07:34 +0100

    create category entity

---
+++ src/main/java/com/stringcodeltd/myblogapp/model/Category.java
@@ -0,0 +1,22 @@
+package com.stringcodeltd.myblogapp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+}
