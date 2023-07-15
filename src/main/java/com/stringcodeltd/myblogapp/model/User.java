c50d4444c56b2a68e049f33ee031add6b3541342
Adebayo Michael <mcgamma04@yahoo.com>
2023-07-15 11:53:23 +0100

    makrked it with entity

--- src/main/java/com/stringcodeltd/myblogapp/model/User.java
+++ src/main/java/com/stringcodeltd/myblogapp/model/User.java
@@ -1,10 +1,26 @@
 package com.stringcodeltd.myblogapp.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Column(nullable = false, unique = true)
     private String username;
+    @Column(nullable = false, unique = true)
     private String email;
+    @Column(nullable = false, unique = true)
     private String password;
 }
