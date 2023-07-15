e0023bf9035b7d4c40a33a41116b73861cd6f72e
Adebayo Michael <mcgamma04@yahoo.com>
2023-07-15 11:40:41 +0100

    created User model

---
+++ src/main/java/com/stringcodeltd/myblogapp/model/User.java
@@ -0,0 +1,10 @@
+package com.stringcodeltd.myblogapp.model;
+
+
+public class User {
+    private Long id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+}
