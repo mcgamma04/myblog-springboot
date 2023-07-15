45fac3427437983c017264f500c1080254137e83
Adebayo Michael <mcgamma04@yahoo.com>
2023-07-15 09:02:40 +0100

    handled access denied exception

--- src/main/java/com/stringcodeltd/myblogapp/controller/PostController.java
+++ src/main/java/com/stringcodeltd/myblogapp/controller/PostController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,7 +22,7 @@ public class PostController {
         this.postService = postService;
     }
 
-
+  @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<PostDTO> createPost(@Valid @RequestBody PostDTO postDTO) {
      return  new ResponseEntity<>(postService.createPost(postDTO), HttpStatus.CREATED);
@@ -40,12 +41,14 @@ public class PostController {
         return ResponseEntity.ok( postService.getPostById(id));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<PostDTO> updatePost(@PathVariable("id")long id, @Valid @RequestBody PostDTO postDTO){
         PostDTO postFeedBack = postService.updatePost(postDTO, id);
         return new ResponseEntity<>(postFeedBack,HttpStatus.OK);
     }
-@DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/{id}")
     public ResponseEntity<String> deletePost(@PathVariable("id") long id){
     postService.deletePost(id);
         return new ResponseEntity<>("Post deleted successfully",HttpStatus.OK);
--- src/main/java/com/stringcodeltd/myblogapp/exception/GlobalException.java
+++ src/main/java/com/stringcodeltd/myblogapp/exception/GlobalException.java
@@ -5,12 +5,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
 
 import java.util.Date;
 import java.util.HashMap;
@@ -37,6 +39,12 @@ public class GlobalException extends ResponseEntityExceptionHandler {
     public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception,WebRequest webRequest){
         ErrorDetails errorDetails = new ErrorDetails(new Date(),exception.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorDetails> handleRAccessDeniedException(AccessDeniedException exception, WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(),exception.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(CategoryAleadyExistException.class)
