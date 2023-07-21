9e147aeebc548d11447ad24dd72a40e6b192d5c8
Adebayo Michael <mcgamma04@yahoo.com>
2023-07-21 07:11:06 +0100

    added JwtResonseDTO

--- src/main/java/com/stringcodeltd/myblogapp/model/User.java
+++ src/main/java/com/stringcodeltd/myblogapp/model/User.java
@@ -1,6 +1,8 @@
 package com.stringcodeltd.myblogapp.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,6 +24,8 @@ public class User {
     @Column(nullable = false, unique = true)
     private String username;
     @Column(nullable = false, unique = true)
+    @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}",
+            flags = Pattern.Flag.CASE_INSENSITIVE)
     private String email;
     private String password;
 
